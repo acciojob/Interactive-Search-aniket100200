2ba7e11244200fc7c73da6b46fb44a7d730a94c1
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-21 10:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //your JS code here. If required.
 
 function fun() {
-	
+	let arr=document.getElementsByClassName("search");
+	let div=arr[0];
+	let input=arr[1];
+	let button=arr[2];
+
+	div.style="height:40px";
+	input.style="height:30px; font-size:20px";
 }
